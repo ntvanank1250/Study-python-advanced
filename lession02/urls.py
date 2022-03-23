1377684c95ce11ccb71da0b4f926584a8f51b22d
ntvanank1250 <doduyhieu12091998@gmail.com>
2022-03-23 15:42:12 +0700

    Done lession02

--- lession02/urls.py
+++ lession02/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path,include
 from . import views
+
+app_name="lession02"
+
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('detail/<int:person_id>', views.detail, name='detail'),
+    path('forminput/', views.formInput, name='formInput'),
+    path('thankyou/', views.thankYou, name='thankYou'),
+
 ]
--- lession02/views.py
+++ lession02/views.py
@@ -3,15 +3,32 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Person
+from .form import PersonForm
 
 def index(request):
-    all_people = Person.objects.order_by('id')[:10]
+    all_people = Person.objects.order_by('id')[:1000]
     context = {'all_people': all_people}
     return render(request, 'lession02/index.html', context)
 
-# def detail(request, person_id):
-#     try:
-#         person = Person.objects.get(pk=person_id)
-#     except Person.DoesNotExist:
-#         raise Http404("Person does not exist")
-#     return render(request, 'assign_02_app/detail.html', {'person': person})
+def detail(request, person_id):
+    try:
+        infoPerson = Person.objects.get(pk=person_id)
+    except Person.DoesNotExist:
+        raise Http404("Person does not exist")
+    return render(request, 'lession02/detail.html', {'infoPerson': infoPerson})
+
+def formInput(request):
+    context = {}
+
+    form = PersonForm(request.POST or None, request.FILES or None)
+
+  
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/lession02/thankyou/')
+
+    context['form'] = form
+    return render(request, "lession02/formInput.html", context)
+
+def thankYou(request):
+    return render(request,"lession02/thankyou.html")
